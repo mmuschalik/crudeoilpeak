785a4fc54c4be241290afac9504290eb2fbeba34
Maurice Muschalik <maurice@actio.com.au>
2022-06-23 13:49:48 +1000

    add docker build stuff

---
+++ src/build.sh
@@ -0,0 +1,9 @@
+git clone git@github.com:mmuschalik/crudeoilpeak.git
+cd crudeoilpeak/src
+
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 193174210882.dkr.ecr.ap-southeast-2.amazonaws.com
+
+docker build -t nemlog .
+docker tag nemlog:latest 193174210882.dkr.ecr.ap-southeast-2.amazonaws.com/nemlog:latest
+
+docker push 193174210882.dkr.ecr.ap-southeast-2.amazonaws.com/nemlog:latest
---
+++ src/download.sh
@@ -0,0 +1,6 @@
+git clone git@github.com:mmuschalik/crudeoilpeak.git
+cd crudeoilpeak
+mkdir -p data
+curl http://nemlog.com.au/api/stns/nsw/csv >> data/nsw.csv
+git commit -a -m "new run..."
+git push

c1a62f9f37a563974b0625f28f89060c4d2b758f
Maurice Muschalik <maurice@actio.com.au>
2022-06-26 09:04:39 +1000

    ensure data file is added to git

--- src/download.sh
+++ src/download.sh
@@ -2,5 +2,6 @@ git clone git@github.com:mmuschalik/crudeoilpeak.git
 cd crudeoilpeak
 mkdir -p data
 curl http://nemlog.com.au/api/stns/nsw/csv >> data/nsw.csv
+git add data/nsw.csv
 git commit -a -m "new run..."
 git push

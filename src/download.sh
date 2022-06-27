137fcbfc5cb2c8d01387136fa7e51da1978a87d9
Maurice M <maurice.muschalik@gmail.com>
2022-06-27 22:41:19 +0000

    new run...

--- src/download.sh
+++ src/download.sh
@@ -1,4 +1,4 @@
-git clone git@github.com:mmuschalik/crudeoilpeak.git
+#git clone git@github.com:mmuschalik/crudeoilpeak.git
 cd crudeoilpeak
 mkdir -p data
 cd data
@@ -7,9 +7,9 @@ LATEST=$(ls -tp nsw*.csv | grep -v /$ | head -1)
 CURRENT=nsw
 
 curl http://nemlog.com.au/api/stns/nsw/csv >> $CURRENT
-python merge_result.py $LATEST $CURRENT
-rm nsw
+python ../src/merge_result.py $LATEST $CURRENT
+#rm nsw
 
-git add --all
-git commit -a -m "new run..."
-git push
+#git add --all
+#git commit -a -m "new run..."
+#git push

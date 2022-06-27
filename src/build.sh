eca602498350612ddd1de819bcabe64276821589
Maurice Muschalik <maurice@actio.com.au>
2022-06-28 09:48:00 +1000

    update base image

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,8 @@
 #git clone git@github.com:mmuschalik/crudeoilpeak.git
 #cd crudeoilpeak/src
 
+set -e
+
 aws ecr get-login-password --region ap-southeast-2 --profile crudeoilpeak | docker login --username AWS --password-stdin 193174210882.dkr.ecr.ap-southeast-2.amazonaws.com
 
 docker build -t nemlog .
--- src/download.sh
+++ src/download.sh
@@ -3,11 +3,11 @@ cd crudeoilpeak
 mkdir -p data
 cd data
 
-LATEST=$(find ./nsw*.csv -type f -printf "%t - %p\n" | sort -n | tail -1 | awk '{print $7}')
+LATEST=$(ls -tp nsw*.csv | grep -v /$ | head -1)
 CURRENT=nsw
 
 curl http://nemlog.com.au/api/stns/nsw/csv >> $CURRENT
-python merge_result.py $LATEST $CURRENT
+python ../src/merge_result.py $LATEST $CURRENT
 rm nsw
 
 git add --all

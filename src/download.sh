9886f138cd8a7e087a3190aa8ae9b50cd48658eb
Maurice Muschalik <maurice@actio.com.au>
2022-06-28 11:38:45 +1000

    add git back

--- src/download.sh
+++ src/download.sh
@@ -1,4 +1,4 @@
-#git clone git@github.com:mmuschalik/crudeoilpeak.git
+git clone git@github.com:mmuschalik/crudeoilpeak.git
 cd crudeoilpeak
 mkdir -p data
 cd data
@@ -10,6 +10,6 @@ curl http://nemlog.com.au/api/stns/nsw/csv >> $CURRENT
 python ../src/merge_result.py $LATEST $CURRENT
 rm nsw
 
-#git add --all
-#git commit -a -m "new run..."
-#git push
+git add --all
+git commit -a -m "new run..."
+git push

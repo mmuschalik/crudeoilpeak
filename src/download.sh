58c2cd15f3fb9814ddf718d1bcbe9034a5ef5187
Maurice Muschalik <maurice@actio.com.au>
2022-07-03 11:50:14 +1000

    do in parallel

--- src/download.sh
+++ src/download.sh
@@ -5,11 +5,13 @@ cd crudeoilpeak
 mkdir -p data
 cd data
 
-../src/download_state.sh nsw
-../src/download_state.sh qld
-../src/download_state.sh sa
-../src/download_state.sh tas
-../src/download_state.sh vic
+../src/download_state.sh nsw &
+../src/download_state.sh qld &
+../src/download_state.sh sa &
+../src/download_state.sh tas &
+../src/download_state.sh vic &
+
+wait
 
 git add --all
 git commit -a -m "new run..."

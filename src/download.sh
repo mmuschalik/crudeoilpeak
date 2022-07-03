69fa40a080a96c7175a6a80493c907daf87534c3
Maurice Muschalik <maurice@actio.com.au>
2022-07-03 11:36:09 +1000

    add qld

--- src/download.sh
+++ src/download.sh
@@ -1,7 +1,13 @@
+set -e
+
 git clone git@github.com:mmuschalik/crudeoilpeak.git
 cd crudeoilpeak
 mkdir -p data
 cd data
+
+../src/download_state.sh nsw
+../src/download_state.sh qld
+
 
 LATEST=$(ls -1 nsw_202*.csv | sort -r | grep -v /$ | head -1)
 CURRENT=nsw

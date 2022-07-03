b167ed0fa09a0061b67f44bca69e08f006160e15
Maurice Muschalik <maurice@actio.com.au>
2022-07-03 11:41:07 +1000

    update qld

--- src/download.sh
+++ src/download.sh
@@ -8,14 +8,6 @@ cd data
 ../src/download_state.sh nsw
 ../src/download_state.sh qld
 
-
-LATEST=$(ls -1 nsw_202*.csv | sort -r | grep -v /$ | head -1)
-CURRENT=nsw
-
-curl http://nemlog.com.au/api/stns/nsw/csv >> $CURRENT
-python ../src/merge_result.py $LATEST $CURRENT
-rm nsw
-
 git add --all
 git commit -a -m "new run..."
 git push

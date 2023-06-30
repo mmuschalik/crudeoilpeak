d01072bf7911e2fa42a8d4b09dd2b93d78d021b0
Maurice Muschalik <maurice.muschalik@gmail.com>
2023-07-01 02:10:32 +1000

    fix bug with merge

--- src/download_state.sh
+++ src/download_state.sh
@@ -1,5 +1,5 @@
 STATE=$1
-LATEST=$(ls -1 ${STATE}_2022*.csv | sort -r | grep -v /$ | head -1)
+LATEST=$(ls -1 ${STATE}_202?*.csv | sort -r | grep -v /$ | head -1)
 
 curl http://nemlog.com.au/api/stns/${STATE}/csv >> ${STATE}
 python ../src/merge_result.py $LATEST ${STATE}
